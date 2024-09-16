4b43ff3a95bcf3f55b0d7010c25e3546c1322ec1
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-16 14:21:29 +0000

    Save Assignment

--- PP1_2.py
+++ PP1_2.py
@@ -4,17 +4,48 @@
 
 def q1():
 
+    name = 'Alex'
+    print(name)
+
 def q2():
+
+    num1 = 3
+    num2 = 5
+    num3 = 2
+    ans = num1 + num2 + num3
+    print(ans)
 
 def q3():
 
+    int1 = 21
+    int2 = 4
+    ans2 = int1 // int2
+    print(ans2)
+
 def q4():
+
+    quotient1 = 6.3
+    quotient2 = 3.0
+    ans3 = quotient1 / quotient2
+    print(ans3)
 
 def q5():
 
+    message = 'Hello World!'
+    print(message)
+    message = 'Goodbye World!'
+    print(message)
+
 def q6():
 
+    torf = True
+    print(torf)
+
 def q7():
+
+    value1 = 7
+    value2 = 2
+    print(value1 % value2)
 
 q1()
 q2()
